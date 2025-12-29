c6b12fc19f1699efd394013d3e1b1595d1a8fd7f
madhuvmn <madhu.vmn@gmail.com>
2025-12-29 16:57:42 +0530

    Test Files

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, send_file
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Table
+from reportlab.lib.styles import getSampleStyleSheet
+import io
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "PDF Report Service is running"
+
+@app.route("/generate-pdf")
+def generate_pdf():
+    buffer = io.BytesIO()
+
+    doc = SimpleDocTemplate(buffer)
+    styles = getSampleStyleSheet()
+    elements = []
+
+    elements.append(Paragraph("Monthly Report", styles["Title"]))
+    elements.append(Paragraph("Generated using Python only", styles["Normal"]))
+
+    table_data = [
+        ["ID", "Name", "Amount"],
+        ["1", "Madhu", "5000"],
+        ["2", "Anil", "4200"]
+    ]
+
+    elements.append(Table(table_data))
+    doc.build(elements)
+
+    buffer.seek(0)
+
+    return send_file(
+        buffer,
+        as_attachment=True,
+        download_name="report.pdf",
+        mimetype="application/pdf"
+    )
+
+if __name__ == "__main__":
+    app.run()
